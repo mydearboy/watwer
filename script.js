164d243f064b7c40594cc9541ba8bcd26e90a0ba
mydearboy <146511395+mydearboy@users.noreply.github.com>
2023-10-13 15:29:08 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let text = document.getElementById('text');
+let hill1 = document.getElementById('hill1');
+let hill2 = document.getElementById('hill2');
+let hill3 = document.getElementById('hill3');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+let leaf = document.getElementById('leaf');
+let plant = document.getElementById('plant');
+let tree = document.getElementById('tree');
+window.addEventListener('scroll', () =>{
+    let value = window.scrollY;
+    text.style.marginTop = value * 0.75 + 'px';
+    leaf.style.top = value * -0.5 + 'px';
+    leaf.style.left = value * 1.0 + 'px';
+    hill5.style.left = value * 0.5 + 'px';
+    hill4.style.left = value * -0.5 + 'px';
+    hill1.style.top = value * 0.25 + 'px';
+    plant.style.right = value * 2.25 + 'px';
+    tree.style.marginTop = value * -10.5 + 'px';
+});
